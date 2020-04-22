5842abfb7eed645e4682d71f30de0e7592e46232
Farrukh <farruh1996@gmail.com>
2020-04-21 19:24:26 -0700

    Add array to RB converter

--- 0x00-red_black_tree/3-array_to_rb_tree.c
+++ 0x00-red_black_tree/3-array_to_rb_tree.c
@@ -1,1 +1,23 @@
-#!/usr/bin/env bash
+#include "rb_trees.h"
+
+/**
+ * array_to_rb_tree - Convert array to RB tree, O(nlogn) time complexity
+ * @array: array to be converted to RB tree
+ * @size: size of the array
+ * Return: new RB tree consturcted from @array
+*/
+rb_tree_t *array_to_rb_tree(int *array, size_t size)
+{
+	rb_tree_t *root = NULL;
+	size_t i;
+
+	if (!array)
+		return (NULL);
+
+	for (i = 0; i < size; ++i)
+	{
+		rb_tree_insert(&root, array[i]);
+	}
+
+	return (root);
+}
