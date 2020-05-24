1c073da8654ee72ffdef349441cce43095859fd1
Farrukh <farruh1996@gmail.com>
2020-05-23 18:35:24 -0700

    Added task 1

---
+++ 0x01-graphs/1-graph_add_vertex.c
@@ -0,0 +1,48 @@
+#include "graphs.h"
+
+/**
+ * graph_add_vertex - adds a new vertex to @graph
+ * @graph: graph
+ * @str: content of a new vertex
+ * Return: pointer to the created vertex, or NULL on failure
+ */
+vertex_t *graph_add_vertex(graph_t *graph, const char *str)
+{
+	vertex_t *vertex, *new;
+
+	if (!graph || !str)
+		return (NULL);
+	vertex = graph->vertices;
+	while (vertex && vertex->next)
+	{
+		if (!strcmp(vertex->content, str))
+			return (NULL);
+		vertex = vertex->next;
+	}
+
+	new = malloc(sizeof(*new));
+	if (!new)
+		return (NULL);
+	new->content = strdup(str);
+	if (!new->content)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->nb_edges = 0;
+	new->edges = NULL;
+	new->next = NULL;
+
+	graph->nb_vertices += 1;
+	if (!vertex)
+	{
+		new->index = 0;
+		graph->vertices = new;
+	}
+	else
+	{
+		new->index = vertex->index + 1;
+		vertex->next = new;
+	}
+	return (new);
+}
