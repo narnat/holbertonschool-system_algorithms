cbacac159198919422abbae66234dd59e85c7c58
Farrukh <farruh1996@gmail.com>
2020-05-31 15:38:48 -0700

    Remove condition where action is NULL

--- 0x01-graphs/4-depth_first_traverse.c
+++ 0x01-graphs/4-depth_first_traverse.c
@@ -61,7 +61,7 @@ size_t depth_first_traverse(
 	size_t depth = 0, tmp;
 	vertex_t *v;
 
-	if (!graph || !action)
+	if (!graph)
 		return (0);
 	seen = calloc(graph->nb_vertices, sizeof(*seen));
 	if (!seen)
