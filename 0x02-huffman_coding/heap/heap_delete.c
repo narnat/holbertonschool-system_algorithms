39051443c3dfcaa2bb5e7061969e7bec7479b914
Farrukh <farruh1996@gmail.com>
2020-06-24 17:49:15 -0700

    Pass free as free_data

--- 0x02-huffman_coding/heap/heap_delete.c
+++ 0x02-huffman_coding/heap/heap_delete.c
@@ -4,7 +4,7 @@
  * free_tree - free binary heap
  * @root: root
  * @free_data: function to free nodes
-*/
+ */
 void free_tree(binary_tree_node_t *root, void (*free_data)(void *))
 {
 	if (!root)
@@ -18,11 +18,13 @@ void free_tree(binary_tree_node_t *root, void (*free_data)(void *))
  * heap_delete - deletes heap
  * @heap: heap
  * @free_data: function to free nodes
-*/
+ */
 void heap_delete(heap_t *heap, void (*free_data)(void *))
 {
-	if (!free_data || !heap)
+	/* if (!free_data || !heap) */
+	/*	return; */
+	if (!heap)
 		return;
-	free_tree(heap->root, free_data);
+	free_tree(heap->root, free);
 	free(heap);
 }
