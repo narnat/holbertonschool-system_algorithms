585bce66a3c017f49860b0ac933d08d50bc578b2
Farrukh <farruh1996@gmail.com>
2020-06-24 17:45:57 -0700

    Add heap delete

---
+++ 0x02-huffman_coding/heap/heap_delete.c
@@ -0,0 +1,28 @@
+#include "heap.h"
+
+/**
+ * free_tree - free binary heap
+ * @root: root
+ * @free_data - function to free nodes
+*/
+void free_tree(binary_tree_node_t *root, void (*free_data)(void *))
+{
+	if (!root)
+		return;
+	free_tree(root->left, free_data);
+	free_tree(root->right, free_data);
+	free_data(root);
+}
+
+/**
+ * heap_delete - deletes heap
+ * @heap: heap
+ * @free_data - function to free nodes
+*/
+void heap_delete(heap_t *heap, void (*free_data)(void *))
+{
+	if (!free_data || !heap)
+		return;
+	free_tree(heap->root, free_data);
+	free(heap);
+}
