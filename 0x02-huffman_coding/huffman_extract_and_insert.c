d83b3d35a34049201c74d7eeda87878f73079716
Farrukh <farruh1996@gmail.com>
2020-06-25 22:03:47 -0700

    using different extract and insert function

--- 0x02-huffman_coding/huffman_extract_and_insert.c
+++ 0x02-huffman_coding/huffman_extract_and_insert.c
@@ -102,7 +102,7 @@ void *heap_extract_2(heap_t *heap)
  *
  * Return: 1 on success, 0 on failure
  */
-int huffman_extract_and_insert(heap_t *priority_queue)
+int huffman_extract_and_insert_2(heap_t *priority_queue)
 {
 	binary_tree_node_t *new_n, *left, *right;
 	symbol_t *new_s;
@@ -143,5 +143,43 @@ int huffman_extract_and_insert(heap_t *priority_queue)
 	new_s->freq = freq;
 	new_s->data = -1;
 	heap_insert(priority_queue, (void *)new_n);
+	return (1);
+}
+
+/**
+ * huffman_extract_and_insert - creates huffman tree
+ * @priority_queue: priority queue which needs to be
+ * converted into huffman tree
+ * Return: 1 on success or 0 on failure
+*/
+int huffman_extract_and_insert(heap_t *priority_queue)
+{
+	binary_tree_node_t *node1, *node2, *new;
+	symbol_t *sym;
+	int freq = 0;
+
+	/* TODO: do not extract nodes with data == -1 */
+	if (!priority_queue || priority_queue->size < 2)
+		return (0);
+	node1 = heap_extract_2(priority_queue);
+	node2 = heap_extract_2(priority_queue);
+	if (node1)
+		freq += ((symbol_t *)node1->data)->freq;
+	if (node2)
+		freq += ((symbol_t *)node2->data)->freq;
+	sym = symbol_create(-1, freq);
+	if (!sym)
+		return (0);
+	new = binary_tree_node(NULL, (void *)sym);
+	if (!new)
+		return (0);
+	new->left = node1;
+	new->right = node2;
+	if (node1)
+		node1->parent = new;
+	if (node2)
+		node2->parent = new;
+	if (!heap_insert(priority_queue, (void *)new))
+		return (0);
 	return (1);
 }
