a5b8635f3967e2fafa41b417c00647381ca72d2d
Farrukh <farruh1996@gmail.com>
2020-06-25 22:35:44 -0700

    Revert function

--- 0x02-huffman_coding/heap/heap_extract.c
+++ 0x02-huffman_coding/heap/heap_extract.c
@@ -1,33 +1,6 @@
 #include "heap.h"
 
 /**
- * get_last_node - get the parent where next node should be inserted
- * @node: heap
- * @n: index of node to be inserted
- * Return: parent node which will store the next inserted node
- */
-binary_tree_node_t *get_last_node(binary_tree_node_t *node, size_t n)
-{
-	int index = 0, mask;
-
-	for (index = 0; (1 << (index + 1)) <= (int)n; ++index)
-		;
-	for (--index; index >= 0; --index)
-	{
-		mask = 1 << index;
-		if (n & mask)
-		{
-			node = node->right;
-		}
-		else
-		{
-			node = node->left;
-		}
-	}
-	return (node);
-}
-
-/**
  * swap - swap data of two nodes
  * @n1: node1
  * @n2: node2
@@ -78,8 +51,6 @@ void sift_down(heap_t *heap, binary_tree_node_t *node)
 	}
 }
 
-binary_tree_node_t *get_last_parent(binary_tree_node_t *node, size_t n);
-
 /**
  * heap_extract - extracts root node from heap (min value)
  * @heap: heap
@@ -100,12 +71,19 @@ void *heap_extract(heap_t *heap)
 		heap->size = 0;
 		return (data);
 	}
-	last_node = get_last_node(heap->root, heap->size);
+	last_node = get_nth_node(heap->root, heap->size);
 	heap->root->data = last_node->data;
-	if (last_node->parent->left == last_node)
-		last_node->parent->left = NULL;
+	if (last_node->parent)
+	{
+		if (last_node->parent->left == last_node)
+			last_node->parent->left = NULL;
+		else
+			last_node->parent->right = NULL;
+	}
 	else
-		last_node->parent->right = NULL;
+	{
+		heap->root = NULL;
+	}
 	free(last_node);
 	heap->size -= 1;
 	/* sift_down(heap, heap->root); */
