73e2bb5917e50159ee9a377b542421e06a4ee7ea
Farrukh <farruh1996@gmail.com>
2020-06-26 22:58:40 -0700

    Initialize data

--- 0x02-huffman_coding/symbol.c
+++ 0x02-huffman_coding/symbol.c
@@ -10,7 +10,7 @@ symbol_t *symbol_create(char data, size_t freq)
 {
 	symbol_t *sym;
 
-	sym = malloc(sizeof(*sym));
+	sym = calloc(sizeof(*sym), 1);
 	if (!sym)
 		return (NULL);
 	sym->data = data;
