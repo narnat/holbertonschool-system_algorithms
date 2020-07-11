0bf4c217518da120a413a0fd9c84d179ecba00ab
Farrukh <farruh1996@gmail.com>
2020-07-11 14:46:29 -0700

    Add version 2 of task 0, optimized memory use

--- 0x03-pathfinding/0-backtracking_array.c
+++ 0x03-pathfinding/0-backtracking_array.c
@@ -2,58 +2,23 @@
 #include "queues.h"
 
 /**
- * create_point - creates a point
+ * add_point - creates a point and adds to a queue
+ * @queue: queue
  * @x: X coordinate
  * @y: Y coordinate
- * Return: a new point or NULL if failed
-*/
-point_t *create_point(int x, int y)
+ * Return: 1 if success, 0 if failed
+ */
+int add_point(queue_t **queue, int x, int y)
 {
 	point_t *p;
 
 	p = malloc(sizeof(*p));
 	if (!p)
-		return (NULL);
+		return (0);
 	p->x = x;
 	p->y = y;
-	return (p);
-}
-
-/**
- * merge_queue - merges two queues
- * @q1: first queue
- * @q2: second queue
- * Return: @q1 with merged @q2
-*/
-queue_t *merge_queue(queue_t *q1, queue_t *q2)
-{
-	q1->back->next = q2->front;
-	q2->front->prev = q1->back;
-	free(q2);
-	return (q1);
-}
-
-/**
- * queue_free - frees queue and sets its pointer to NULL
- * @q: queue to be freed
-*/
-void queue_free(queue_t **q)
-{
-	queue_node_t *node, *tmp;
-
-	if (*q)
-	{
-		node = (*q)->front;
-		while (node)
-		{
-			tmp = node;
-			node = node->next;
-			free(tmp->ptr);
-			free(tmp);
-		}
-		free(*q);
-	}
-	*q = NULL;
+	queue_push_front(*queue, p);
+	return (1);
 }
 
 /**
@@ -69,44 +34,29 @@ void queue_free(queue_t **q)
  * @target: target point
  * Return: queue with points or NULL if failed
 */
-queue_t *backtracking_helper(queue_t **queue, int *visited, char **map,
-			     int rows, int cols, int x, int y,
+int backtracking_helper(queue_t **queue, int *visited, char **map,
+			int rows, int cols, int x, int y,
 			     point_t const *target)
 {
-	queue_t *q = NULL;
-	point_t *p;
-
 	if (x < 0 || x >= cols || y < 0 || y >= rows ||
 	    map[y][x] == '1' || *(visited + y * cols + x) == 1)
 	{
-		queue_free(queue);
-		return (NULL);
-	}
-	if (!*queue)
-		*queue = queue_create();
-	if (!*queue)
-		return (NULL);
-	p = create_point(x, y);
-	if (!p)
-	{
-		queue_free(queue);
-		return (NULL);
+		return (0);
 	}
 	printf("Checking coordinates [%d, %d]\n", x, y);
-	queue_push_back(*queue, p);
 	if (x == target->x && y == target->y)
-		return (*queue);
+		return (add_point(queue, x, y));
 	*(visited + y * cols + x) = 1;
-	if (backtracking_helper(&q, visited, map, rows, cols, x + 1, y, target))
-		return (merge_queue(*queue, q));
-	if (backtracking_helper(&q, visited, map, rows, cols, x, y + 1, target))
-		return (merge_queue(*queue, q));
-	if (backtracking_helper(&q, visited, map, rows, cols, x - 1, y, target))
-		return (merge_queue(*queue, q));
-	if (backtracking_helper(&q, visited, map, rows, cols, x, y - 1, target))
-		return (merge_queue(*queue, q));
-	*(visited + y * cols + x) = 0, queue_free(queue);
-	return (NULL);
+	if (backtracking_helper(queue, visited, map, rows, cols, x + 1, y, target))
+		return (add_point(queue, x, y));
+	if (backtracking_helper(queue, visited, map, rows, cols, x, y + 1, target))
+		return (add_point(queue, x, y));
+	if (backtracking_helper(queue, visited, map, rows, cols, x - 1, y, target))
+		return (add_point(queue, x, y));
+	if (backtracking_helper(queue, visited, map, rows, cols, x, y - 1, target))
+		return (add_point(queue, x, y));
+	*(visited + y * cols + x) = 0;
+	return (0);
 }
 
 /**
@@ -124,13 +74,19 @@ queue_t *backtracking_array(char **map, int rows, int cols,
 {
 	int *visited;
 	queue_t *queue;
+	int ret;
 
 	visited = calloc(rows * cols, sizeof(*visited));
 	queue = queue_create();
 	if (!queue)
 		return (NULL);
-	queue = backtracking_helper(&queue, visited, map, rows, cols,
-				    start->x, start->y, target);
+	ret = backtracking_helper(&queue, visited, map, rows, cols,
+				  start->x, start->y, target);
+	if (!ret)
+	{
+		queue_delete(queue);
+		return (NULL);
+	}
 	free(visited);
 	return (queue);
 }
