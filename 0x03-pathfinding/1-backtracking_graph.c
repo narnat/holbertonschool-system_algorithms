7a770e5f2457bb7928986bbf71d0977ada9e2aca
Farrukh <farruh1996@gmail.com>
2020-07-11 14:30:56 -0700

    Add version 2 of backtracking graph, optmized memory use

--- 0x03-pathfinding/1-backtracking_graph.c
+++ 0x03-pathfinding/1-backtracking_graph.c
@@ -3,43 +3,6 @@
 #include "queues.h"
 
 /**
- * merge_queue - merges two queues
- * @q1: first queue
- * @q2: second queue
- * Return: @q1 with merged @q2
- */
-queue_t *merge_queue(queue_t *q1, queue_t *q2)
-{
-	q1->back->next = q2->front;
-	q2->front->prev = q1->back;
-	free(q2);
-	return (q1);
-}
-
-/**
- * queue_free - frees queue and sets its pointer to NULL
- * @q: queue to be freed
- */
-void queue_free(queue_t **q)
-{
-	queue_node_t *node, *tmp;
-
-	if (*q)
-	{
-		node = (*q)->front;
-		while (node)
-		{
-			tmp = node;
-			node = node->next;
-			free(tmp->ptr);
-			free(tmp);
-		}
-		free(*q);
-	}
-	*q = NULL;
-}
-
-/**
  * backtrack_graph_helper - backtracking graph to find a path, helper function
  * @queue: queue of path so far
  * @seen: seen cities on the current path
@@ -47,36 +10,36 @@ void queue_free(queue_t **q)
  * @target: target city (vertex)
  * Return: queue of full path, NULL if failed
  */
-queue_t *backtrack_graph_helper(queue_t **queue, int *seen, vertex_t const *cur,
-				vertex_t const *target)
+int backtrack_graph_helper(queue_t **queue, int *seen, vertex_t const *cur,
+			   vertex_t const *target)
 {
-	queue_t *q = NULL;
 	edge_t *edge;
+	int ret;
 
 	if (!target || !cur || !seen || seen[cur->index])
 	{
-		queue_free(queue);
-		return (NULL);
+		return (0);
 	}
-	if (!*queue)
-		*queue = queue_create();
-	if (!*queue)
-		return (NULL);
 	printf("Checking %s\n", cur->content);
-	queue_push_back(*queue, strdup(cur->content));
 	if (!strcmp(cur->content, target->content))
-		return (*queue);
+	{
+		queue_push_front(*queue, strdup(cur->content));
+		return (1);
+	}
 	seen[cur->index] = 1;
 	edge = cur->edges;
 	while (edge)
 	{
-		if (backtrack_graph_helper(&q, seen, edge->dest, target))
-			return (merge_queue(*queue, q));
+		ret = backtrack_graph_helper(queue, seen, edge->dest, target);
+		if (ret)
+		{
+			queue_push_front(*queue, strdup(cur->content));
+			return (1);
+		}
 		edge = edge->next;
 	}
 	seen[cur->index] = 0;
-	queue_free(queue);
-	return (NULL);
+	return (0);
 }
 
 /**
@@ -91,13 +54,22 @@ queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
 {
 	int *seen = NULL;
 	queue_t *q = NULL;
+	int s;
 
 	if (!graph || !start || !target)
 		return (NULL);
 	seen = calloc(graph->nb_vertices, sizeof(*seen));
 	if (!seen)
 		return (NULL);
-	q = backtrack_graph_helper(&q, seen, start, target);
+	q = queue_create();
+	if (!q)
+		return (NULL);
+	s = backtrack_graph_helper(&q, seen, start, target);
 	free(seen);
+	if (!s)
+	{
+		queue_delete(q);
+		return (NULL);
+	}
 	return (q);
 }
