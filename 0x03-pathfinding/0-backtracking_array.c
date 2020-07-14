42a74af621a147fc216cbe0cb665e513a79faa98
Farrukh <farruh1996@gmail.com>
2020-07-13 18:02:58 -0700

    Fix a bug on null check

--- 0x03-pathfinding/0-backtracking_array.c
+++ 0x03-pathfinding/0-backtracking_array.c
@@ -75,7 +75,7 @@ queue_t *backtracking_array(char **map, int rows, int cols,
 	queue_t *queue;
 	int ret;
 
-	if (!map || !*map || !start || target || rows < 1 || cols < 1)
+	if (!map || !*map || !start || !target || rows < 1 || cols < 1)
 		return (NULL);
 	visited = calloc(rows * cols, sizeof(*visited));
 	if (!visited)
