080059fc20581495b6614da4f76b0bb8390cd3b6
Farrukh Akhrarov <farruh1996@gmail.com>
2020-08-31 18:33:59 -0700

    Add task 4

---
+++ 0x04-nary_trees/4-path_exists.c
@@ -0,0 +1,32 @@
+#include "nary_trees.h"
+
+/**
+* path_exists - check if path exists in a tree
+* @root: root of a tree
+* @path: path to check
+* Return: 1 if path exists, 0 if not
+*/
+int path_exists(nary_tree_t const *root, char const * const *path)
+{
+	int idx = 0;
+	nary_tree_t const *node;
+
+	if (!root || !path)
+		return (0);
+	node = root;
+	while (node)
+	{
+		if (!path[idx])
+			return (0);
+		while (node && strcmp(path[idx], node->content))
+			node = node->next;
+		if (!node)
+			return (0);
+		node = node->children;
+		++idx;
+		if (!path[idx])
+			break;
+	}
+
+	return (!path[idx]);
+}
